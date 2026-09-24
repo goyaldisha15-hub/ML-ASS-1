b85adef5b64def5a61932f669bdd0ebb4c361bb8
goyaldisha15-hub <goyaldisha15-hub@users.noreply.github.com>
2026-09-24 12:34:00 +0530

    Remove Student management folder

--- OneDrive/backend-pr/Student-management/routes/studentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require ('express');
-const router = express.Router();
-const students = require('../data/students');
-
-
-router.get('/students', (req, res) => {
-    res.status(200).json(students);
-});
